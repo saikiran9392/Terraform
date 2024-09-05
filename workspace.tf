74ecf9e5ee37a2d03cf1b2320bd60542328908a8
saikiran9392 <saikiranveeravarapu@gmail.com>
2024-09-05 23:57:18 +0530

    Update workspace.tf

--- workspace.tf
+++ workspace.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 locals {
-  instance_name="${terraform.workspace}-instance"
+  instance_name="${terraform.workspace}-instance"  # use interpolation concept to display the current workspace while executing
 }
 
 resource "aws_instance" "demo" {
@@ -21,3 +21,8 @@ resource "aws_instance" "demo" {
     Name = local.instance_name
   }
 }
+
+# terraform workspace list -> To list workspaces
+# terraform workspace new dev -> To Create workspace
+# terraform workspace show -> To dispaly current workspace
+# terraform workspace select dev -> To switch workspace
